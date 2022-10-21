a7d2c5a26dd30ac2b3adb4fc302a1621d54f4f94
rainweb82 <105602643+rainweb82@users.noreply.github.com>
2022-10-21 16:15:32 +0800

    Delete mail.sh

--- mail.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-NUM=$(/bin/bash check.sh |grep -e 200 -e 000 | wc -l)
-NUMM=$(cat url.list |wc -l)
-if [ $NUM -ne  $NUMM ]
-then
-mail -s "Service Confirm - Critical" xxx@163.com < result.log 
-else
-mail -s "Service Confirm - ok" xxx@163.com < result.log 
-fi
